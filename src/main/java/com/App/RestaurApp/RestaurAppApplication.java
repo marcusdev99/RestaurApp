1a630327e29599e87b00f4d71f10c6fc81726448
Marcus Gonzalez <marcus.sea13@gmail.com>
2022-08-17 18:58:34 -0500

    Creacion de estructura de proyecto Spring boot "RestaurApp"

---
+++ src/main/java/com/App/RestaurApp/RestaurAppApplication.java
@@ -0,0 +1,13 @@
+package com.App.RestaurApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurAppApplication.class, args);
+	}
+
+}
