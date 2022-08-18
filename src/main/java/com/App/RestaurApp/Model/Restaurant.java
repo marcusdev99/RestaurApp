896c3216b2d82104d73b3b3168f162d992500f67
Marcus Gonzalez <marcus.sea13@gmail.com>
2022-08-17 22:11:14 -0500

    Creacion de la clase Restaurante.

---
+++ src/main/java/com/App/RestaurApp/Model/Restaurant.java
@@ -0,0 +1,76 @@
+package com.App.RestaurApp.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String description;
+    private String history;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFounder() {
+        return founder;
+    }
+
+    public void setFounder(String founder) {
+        this.founder = founder;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    private String address;
+    private String founder;
+    private String year;
+    
+    
+}
