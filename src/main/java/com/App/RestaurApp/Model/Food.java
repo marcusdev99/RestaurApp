01ddafd1cea4a530ad079b7513deb0e85456d08a
Marcus Gonzalez <marcus.sea13@gmail.com>
2022-08-17 22:23:49 -0500

    Insercion de metodos getter and setter.

--- src/main/java/com/App/RestaurApp/Model/Food.java
+++ src/main/java/com/App/RestaurApp/Model/Food.java
@@ -14,5 +14,47 @@ public class Food {
     private String price;
     @ManyToOne
     private Restaurant restaurant;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+    
+    
     
 }
